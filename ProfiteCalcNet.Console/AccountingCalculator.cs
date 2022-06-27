9b949aee64ef6f69e74d5fee01d1e1699b526e4e
Richard Hessinger <rhessinger@inedo.com>
2022-06-27 16:09:16 -0400

    Initial ProfitCalc .NET 6 commit

---
+++ ProfiteCalcNet.Console/AccountingCalculator.cs
@@ -0,0 +1,23 @@
+﻿namespace ProfiteCalcNet6.Console
+{
+    /// <summary>
+    /// Calculator used determine net cost.
+    /// </summary>
+    public sealed class AccountingCalculator
+    {
+        /// <summary>
+        /// Calculates the net.
+        /// </summary>
+        /// <param name="revenue">The revenue.</param>
+        /// <param name="expenses">The expenses.</param>
+        public decimal CalculateNet(decimal revenue, decimal expenses)
+        {
+            if (revenue < 0)
+                throw new ArgumentOutOfRangeException("revenue");
+            if (expenses < 0)
+                throw new ArgumentOutOfRangeException("expenses");
+
+            return revenue - expenses;
+        }
+    }
+}
---
+++ ProfiteCalcNet.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using ProfiteCalcNet6.Console;
+
+decimal? revenue = null;
+decimal? expenses = null;
+
+if (args.Length == 2)
+{
+    revenue = decimal.TryParse(args[0], out decimal r) ? (decimal?)r : null;
+    expenses = decimal.TryParse(args[1], out decimal e) ? (decimal?)e : null;
+}
+
+if (revenue == null || expenses == null)
+{
+    revenue = PromptDecimal("Enter revenue: ");
+    expenses = PromptDecimal("Enter expenses: ");
+}
+
+decimal profit = new AccountingCalculator().CalculateNet(revenue.Value, expenses.Value);
+
+Console.WriteLine("Profit: " + profit.ToString("C2"));
+Console.WriteLine("Profit JSON: " + JsonConvert.SerializeObject(profit));
+
+static decimal PromptDecimal(string prompt)
+{
+    while (true)
+    {
+        Console.Write(prompt);
+        var input = Console.ReadLine();
+        if (decimal.TryParse(input, out decimal d))
+            return d;
+        else
+            Console.WriteLine("Please enter a valid decimal value.");
+    }
+}
